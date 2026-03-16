9d7184d6662884c659741c16baaddc3f3497e1b0
B1m-b0M <pas.naario@gmail.com>
2026-03-16 21:30:53 +0200

    Practice 6: Number Line Jumps

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,18 @@
 // https://www.hackerrank.com/challenges/kangaroo/problem
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> bool {
+    if v1 <= v2 {
+        return false;
+    }
+
+    (x2 - x1) % (v1 - v2) == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_kangaroo() {
+        assert_eq!(kangaroo(0, 3, 4, 2), true);
+    }
+}
