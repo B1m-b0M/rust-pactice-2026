bd3a43789494f404a6b9a4eec6c0a84e9366e39a
B1m-b0M <pas.naario@gmail.com>
2026-03-16 21:17:58 +0200

    Practice 5: Apple-and-orange

--- src/hackerrank/task05.rs
+++ src/hackerrank/task05.rs
@@ -1,1 +1,39 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
+pub fn count_apples_and_oranges(
+    s: i32,
+    t: i32,
+    a: i32,
+    b: i32,
+    apples: Vec<i32>,
+    oranges: Vec<i32>,
+) -> (i32, i32) {
+    let apple_count = apples
+        .iter()
+        .map(|d| a + d)
+        .filter(|&pos| pos >= s && pos <= t)
+        .count() as i32;
+
+    let orange_count = oranges
+        .iter()
+        .map(|d| b + d)
+        .filter(|&pos| pos >= s && pos <= t)
+        .count() as i32;
+
+    (apple_count, orange_count)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_apples_oranges() {
+        let apples = vec![-2, 2, 1];
+        let oranges = vec![5, -6];
+
+        assert_eq!(
+            count_apples_and_oranges(7, 11, 5, 15, apples, oranges),
+            (1, 1)
+        );
+    }
+}
